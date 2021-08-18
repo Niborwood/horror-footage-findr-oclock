0e9061e38b1af271df63caceeca991b9d4036ea3
DevOfPopie <sophie.seignon@gmail.com>
2021-08-18 09:50:34 +0200

    False data to test in table 'user'

--- data/user.sql
+++ data/user.sql
@@ -1,3 +1,6 @@
 INSERT INTO "user" ("pseudo", "password", "email") VALUES
 
-    ('', '', ''),
+    ('sadelena', 'testillon', 'sadelena@truc.fr'),
+    ('robinozo', 'jkiffhorror', 'robinozo@truc.fr'),
+    ('correntinus', 'tchoutchou', 'correntinus@truc.fr'),
+    ('arnhost', 'intheforest', 'arnhost@truc.fr');
