1cc32f003f6ec9e27daa4451a83d093d695e0518
Niborwood <robin.souriau@gmail.com>
2021-08-23 12:22:10 +0200

    Toggle Tested and dynamic, ready to be used

--- frontHorrorFootageFinder/src/reducers/ui.js
+++ frontHorrorFootageFinder/src/reducers/ui.js
@@ -1,9 +1,14 @@
 import {
   PASS_SPLASH,
+  TOGGLE_ACTION,
 } from '../actions/ui';
 
 const initialState = {
   splashPassed: true,
+  toggles: {
+    toggleAnimations: false,
+    // chaque nouvel état par défaut de toggle s'ajoute ici
+  },
 };
 
 const UIreducer = (state = initialState, action) => {
@@ -12,6 +17,15 @@ const UIreducer = (state = initialState, action) => {
       return {
         ...state,
         splashPassed: true,
+      };
+
+    case TOGGLE_ACTION:
+      return {
+        ...state,
+        toggles: {
+          ...state.toggles,
+          [action.toggleName]: !state.toggles[action.toggleName],
+        },
       };
 
     default:
