bcb84ffb70fa740f2ff92e2c803c948d590ddced
Niborwood <robin.souriau@gmail.com>
2021-08-23 15:04:15 +0200

    Edit CORS v2

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const router = require('./app/routers');
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', ['https://horror-footage-finder.netlify.app/', 'http://localhost:3000']); // A changer avec la future adresse du front ..
+  const allowedOrigins = ['https://horror-footage-finder.netlify.app/', 'http://localhost:3000'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  // res.header('Access-Control-Allow-Origin', ['https://horror-footage-finder.netlify.app/', 'http://localhost:3000']); // A changer avec la future adresse du front ..
   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, OPTIONS, PUT, DELETE');
   // response to preflight request
