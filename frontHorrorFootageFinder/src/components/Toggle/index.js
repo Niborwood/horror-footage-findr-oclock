5c1b7b3bdd03af0c77afccd494f721be2ec97664
Niborwood <robin.souriau@gmail.com>
2021-08-23 12:22:10 +0200

    Toggle Tested and dynamic, ready to be used

--- frontHorrorFootageFinder/src/components/Toggle/index.js
+++ frontHorrorFootageFinder/src/components/Toggle/index.js
@@ -2,22 +2,57 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
-export const Toggle = ({ textContent }) => (
-  <div className="toggle-ui">
+// SCSS
+import './toggle.scss';
+
+// ACTIONS TO DISPATCH
+import { toggleAction } from '../../actions/ui';
+
+// COMPOSANT
+// ------------ Mode d'emploi :
+// Le Composant Toggle doit recevoir deux informations obligatoires :
+// - name : le nom du toggle qui sera stocké dans le store, sous le format : 'toggleAnimations'
+// - textContent : le texte du toggle
+// Tout le reste (enregistrement dans le store, switch true/false)
+// est automatiquement géré par le composant.
+
+// ------------ Important :
+// Lorsque vous créez un composant Toggle :
+// - vous devez entrer son état par défaut dans le store.
+// -> cela s'ajoute dans reducers/ui, au sein de l'objet 'toggles'.
+// -> son nom doit être égal au name indiqué dans le composant.
+export const Toggle = ({
+  textContent, name, triggerToggle, currentToggle,
+}) => (
+  <label htmlFor={name} className={`toggle-ui ${currentToggle && 'toggle-ui__checked'}`}>
+    <input
+      type="checkbox"
+      name={name}
+      id={name}
+      value={textContent}
+      onClick={() => triggerToggle(name)}
+    />
     {textContent}
-  </div>
+  </label>
 );
 
 Toggle.propTypes = {
+  // FROM PARENT
   textContent: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  // FROM STORE
+  triggerToggle: PropTypes.func.isRequired,
+  currentToggle: PropTypes.bool.isRequired,
 };
 
-const mapStateToProps = () => ({
-
+const mapStateToProps = ({ ui: { toggles } }, { name }) => ({
+  currentToggle: toggles[name],
 });
 
-const mapDispatchToProps = {
-
-};
+const mapDispatchToProps = (dispatch) => ({
+  triggerToggle: (toggleName) => {
+    dispatch(toggleAction(toggleName));
+  },
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(Toggle);
