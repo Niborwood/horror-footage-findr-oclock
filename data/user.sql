92aa3b067735bccb041c928c2ded5ffaf94fa6aa
Niborwood <robin.souriau@gmail.com>
2021-08-24 15:23:55 +0200

    Added & Tested routes : movie/:tmdbID(get), quiz(post), searchmovies(get)

--- data/user.sql
+++ data/user.sql
@@ -3,4 +3,6 @@ INSERT INTO "horror_user" ("pseudo", "email", "password") VALUES
     ('sadelena', 'sadelena@truc.fr', 'testillon'),
     ('robinozo', 'robinozo@truc.fr', 'jkiffhorror'),
     ('correntinus', 'correntinus@truc.fr', 'tchoutchou'),
+    ('marius', 'marinus@truc.fr', 'marius'),
+    ('machin', 'machin@truc.fr', 'machin'),
     ('arnhost', 'arnhost@truc.fr', 'intheforest');
