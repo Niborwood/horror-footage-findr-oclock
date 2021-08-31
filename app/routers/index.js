25ae5ae22a3343f1d9d84ded963f064fba8e2c1f
DevOfPopie <sophie.seignon@gmail.com>
2021-08-31 13:22:19 +0200

    Removal of the validate schema to update user

--- app/routers/index.js
+++ app/routers/index.js
@@ -17,8 +17,8 @@ const jwtMiddleware = require('../services/jwt');
 // Mon schema de vérification des infos du user avec joi :
 const validate = require('../validations/validate');
 const userSchema = require('../validations/schema/user');
-const updateUserSchema = require('../validations/schema/updateUser');
-const updatePasswordSchema = require('../validations/schema/updatePassword');
+// const updateUserSchema = require('../validations/schema/updateUser');
+// const updatePasswordSchema = require('../validations/schema/updatePassword');
 
 
 // Les routes :
@@ -44,10 +44,10 @@ router.post('/api/v1/login', userController.userLogged);
 
 router.route('/api/v1/user/:id')
     .get(userController.findUser)
-    .patch(jwtMiddleware.authenticateToken, validate('body update', updateUserSchema), userController.updateUser)
+    .patch(jwtMiddleware.authenticateToken, userController.updateUser) //  validate('body update', updateUserSchema),
     .delete(jwtMiddleware.authenticateToken, userController.deleteUser);
 
-router.patch('/api/v1/user/:id/change', jwtMiddleware.authenticateToken, validate('body update password', updatePasswordSchema), userController.changePasseword);
+router.patch('/api/v1/user/:id/change', jwtMiddleware.authenticateToken, userController.changePasseword); //  validate('body update password', updatePasswordSchema),
 
 router.get('/api/v1/user/:id/details', userController.getAllDetails);
 
