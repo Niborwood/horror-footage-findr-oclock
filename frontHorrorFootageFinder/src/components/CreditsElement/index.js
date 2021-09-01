d8feb7dfc6bcd08ddd32c65440ba2b5402bcf459
ArnaudDelperier <arnaud.delperier@gmail.com>
2021-09-01 18:16:46 +0200

    css on credits wip

---
+++ frontHorrorFootageFinder/src/components/CreditsElement/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import './creditsElement.scss';
+
+export const CreditsElement = ({ text }) => (
+  <div className="credits-element">
+    <div className="credits-element__text">
+      {text}
+    </div>
+  </div>
+);
+
+CreditsElement.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+
+});
+
+const mapDispatchToProps = {
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CreditsElement);
