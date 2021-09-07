fbb0fe8de87fd69163d022fa63317be5274ebcec
Corentin Couzigou <80675299+CorentinCouzigou@users.noreply.github.com>
2021-09-07 11:47:24 +0200

    button for return homepage for the confirm page CHECK

--- frontHorrorFootageFinder/src/components/pageConfirm/index.js
+++ frontHorrorFootageFinder/src/components/pageConfirm/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Header from '../Header';
 import Footer from '../Footer';
+import Button from '../Button';
 import './style.scss';
 
 export default function pageConfirm() {
@@ -10,7 +11,8 @@ export default function pageConfirm() {
       <Header />
       <div className="pageConfirm__container">
         <p className="pageConfirm__text">Un mail de confirmation vous a été envoyé, veuillez le confirmer pour finaliser votre compte.</p>
-        <NavLink className="pageConfirm__link" to="/">Page d&apos;accueil</NavLink>
+        <Button to="/" textContent="Retour à l'accueil" />
+        {/* <NavLink className="pageConfirm__link" to="/">Page d&apos;accueil</NavLink> */}
       </div>
       <Footer />
     </>
