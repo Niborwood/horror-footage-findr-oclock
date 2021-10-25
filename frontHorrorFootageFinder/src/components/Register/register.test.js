49bb63b9f0f5668d3bedff417367d328cde1d47e
Corentin Couzigou <80675299+CorentinCouzigou@users.noreply.github.com>
2021-10-25 09:45:27 +0200

    test in progress

--- frontHorrorFootageFinder/src/components/Register/register.test.js
+++ frontHorrorFootageFinder/src/components/Register/register.test.js
@@ -1,3 +1,21 @@
-it ('somme des nombres', () => {
-expect((2+1)).toEqual(3)
-})
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+import sum from './sum';
+
+it('sums numbers', () => {
+  expect(sum(1, 2)).toEqual(3);
+  expect(sum(2, 2)).toEqual(4);
+});
+// import { toggleMasked, TOGGLE_MASKED } from '../../actions/register';
+
+// const sum = require('./sum');
+
+// const register = require('./index');
+
+// it('should create an action with INCREMENT_BY type', () => {
+//   const expectation = {
+//     type: 'TOGGLE_MASKED',
+//   };
+
+//   expect(toggleMasked()).toEqual(expectation);
+// });
